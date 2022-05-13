9c861882af8c3eef61bdbbd3ae36b47efe12fd37
Christopher Mogis <102024007+ChrisMogis@users.noreply.github.com>
2022-05-13 15:22:12 +0200

    Add files via upload
    Script creation

---
+++ MonitoringServices.ps1
@@ -0,0 +1,58 @@
+﻿#Function create Log folder
+    Function CreateLogsFolder
+{
+    If(!(Test-Path C:\Logs))
+    {
+    New-Item -Force -Path "C:\Logs\" -ItemType Directory
+		}
+		else 
+		{ 
+    Write-Host "Le dossier "C:\Logs\" existe déjà !"
+    }
+}
+
+#Create Log Folder
+    CreateLogsFolder
+
+#Declaration of script variables
+    $Client = "C-QUADRAT"
+    $Server = (Get-CimInstance -ClassName Win32_ComputerSystem).Name
+    $Date = Get-Date
+    $ServiceName = "XboxGipSvc"
+    $LogPath = "C:\Logs\$ServiceName.log"
+
+#Get Gervice Status Information
+    $Service = Get-Service | Where-Object { $_.Name -eq $serviceName }
+
+#Restart service and wait 15s before checking if the service restarted
+ If ($service.status -eq "Stopped")
+ {
+    Start-Service $serviceName
+
+    Write-Output "$($Date) Service Status is $($service.status)" | Tee-Object -FilePath $LogPath -Append
+
+    Start-Sleep -Seconds 15
+ }
+
+#Get Gervice Status Information
+    $Service = Get-Service | Where-Object { $_.Name -eq $serviceName }
+
+    Write-Output "$($Date) Service Status is $($service.status)" | Tee-Object -FilePath $LogPath -Append
+
+#Send email if service still isn't running
+ If ($service.status -eq "Stopped")
+ {
+    Write-Output "$($Date) Sending Alert email..." | Tee-Object -FilePath $LogPath -Append
+    
+    $smtpServer = "cquadrat-fr0c.mail.protection.outlook.com"
+    $msg = new-object Net.Mail.MailMessage
+    $smtp = new-object Net.Mail.SmtpClient($smtpServer)  
+    $msg.From = "noreply@c-quadrat.fr"
+    $msg.To.Add("cmogis@janus-consulting.fr")
+    #$msg.CC.Add("username@domainname.extension")
+    $msg.Attachments.Add("$LogPath")
+    $msg.subject = $Client + " $Server"  + " Service " + $serviceName + " is " + $service.status
+    $msg.body = ([string]$date) + " Service " + $serviceName + " is " + $service.status 
+    $smtp.send($msg)
+}
+    Write-Host "$($Date) Sending Alert email OK"
